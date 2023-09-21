7abe11423f701bb1e99610cb0735f1e188c7a622
lkarki <lucius.karki@mymail.champlain.edu>
2023-09-21 15:59:00 -0400

    Add project files.

---
+++ testing/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Data;
+
+namespace testing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string h = text1.Text;
+            string filePath = @"C:\\Users\lucius.karki\source\repos\testing\testing\test.txt";
+            StreamWriter sw = new StreamWriter(filePath);
+            sw.WriteLine(h);
+            sw.Close();
+        }
+    }
+}
